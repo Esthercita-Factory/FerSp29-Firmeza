2c291ddd7229fb560d3a8eefc0bf8e04cd035dad
Fernando Alonso Santos Puentes <fsantosp29@gmail.com>
2026-09-23 20:39:04 -0500

    feat: initial clean architecture solution setup with xunit testing

---
+++ test/Firmeza.UnitTests/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using Xunit;
+
+namespace Firmeza.UnitTests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void SolutionArchitecture_ShouldInitializeCorrectly()
+    {
+        bool isSetupComplete = true;
+        
+        Assert.True(isSetupComplete);
+    }
+}
